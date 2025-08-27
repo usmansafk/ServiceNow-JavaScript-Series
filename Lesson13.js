cea6057ac17afa3e6ab543ed0809006b19261b29
usmansafk <usman230@hotmail.co.uk>
2025-08-27 19:39:58 +0100

    L13

---
+++ Lesson13.js
@@ -0,0 +1,65 @@
+// lesson13 switch
+
+var level = 5;
+var message = "";
+
+switch (level) {
+  case 0:
+    message = "Empty";
+    break;
+
+  case 1:
+    message = "Low";
+    break;
+
+  case 3:
+    message = "Mid";
+    break;
+
+  case 4:
+    message = "High";
+    break;
+
+  case 5:
+    message = "V High";
+    break;
+
+  default:
+    message = "Uhmm.....";
+}
+
+gs.info(message);
+
+// ------
+
+// L13S02 - Multiple cases
+//
+// Using the switch statement
+var level = 5;
+var message = "";
+
+switch (level) {
+  case 0:
+    message = "Empty";
+    break;
+
+  case 1:
+  case 2:
+    message = "Low";
+    break;
+
+  case 3: // Warning - what's wrong with case 3?
+    message = "Medium";
+  //break;
+  case 4:
+    message = "High";
+    break;
+
+  case 5:
+    message = "Full";
+    break;
+
+  default:
+    message = "Uh-oh!";
+}
+gs.info("Level=" + level + " status=" + message);

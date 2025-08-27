f982ae98980b7f21da1cbc44d8c14b2f75180402
usmansafk <usman230@hotmail.co.uk>
2025-08-27 21:33:52 +0100

    Lesson22.js

---
+++ Lesson22.js
@@ -0,0 +1,34 @@
+//
+// L22S01 - Arrays
+//
+
+// Make a simple array
+//
+// Optional declaration, but not preferred:
+// var list = Array(); .. rarely used
+var list = [];
+list[0] = 1;
+list[1] = 3;
+list[2] = 5;
+gs.info("length of list is: " + list.length);
+
+//
+// L22S02 - Shorter way
+//
+var list2 = [1, 3, 5];
+gs.info("length of list is: " + list2.length);
+
+//
+// L22S03 - Loops and arrays
+//
+var list3 = [1, 3, 5];
+for (var i = 0; i < list3.length; i++) {
+  gs.info("i=" + i + " value=" + list3[i]);
+}
+
+// Slightly better
+var list4 = [1, 3, 5];
+var len = list4.length;
+for (var i = 0; i < len; i++) {
+  gs.info("i=" + i + " value=" + list4[i]);
+}
